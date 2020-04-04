d422ce6124b031731025ac954160daa19419ccb6
Jordan Romero <jordan.t.romero@gmail.com>
2020-04-04 18:53:56 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,32 +1,30 @@
 class Triangle
-attr_accessor :side1, :side2, :side3, :allsides
-
-  def initialize(side1, side2, side3)
-    @allsides = []
-    @side1 = side1
-    @side2 = side2
-    @side3 = side3
+  # write code here	
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
   end
 
   def kind
-
-    if @side1 == @side2 && @side2 == @side3
-      return :equilateral
-    elsif @side1 == @side2 && @side1 + @side2 > @side3 || @side1 == @side3 && @side1 + @side3 > @side2 || @side2 == @side3 && @side2 + @side3 > side1
-      return :isosceles
-    elsif @side1 != @side2 && @side2 != @side3 && @side1 + @side2 > @side3
-      return :scalene
-    else
-      raise TriangleError if
-        @side1 <=0 || @side2 <= 0 || @side3 <= 0
+      if @a <= 0 || @b <= 0 || @c <= 0
+          raise TriangleError
+          puts error.message
+      elsif @a + @b <= @c || @b + @c <= @a || @a + @c <= @b
+        raise TriangleError
+        puts error.message
+      elsif  @a == @b && @b == @c && @a == @c
+        :equilateral
+      elsif @a == @b || @b == @c || @a == @c
+        :isosceles
+      elsif @a != @b && @b != @c && @a != @c
+        :scalene
       end
-    end 
-
-  class TriangleError < StandardError
-    def maessage
-      "not a triangle"
-    end
-
   end
 
-end
+  class TriangleError < StandardError
+    def message
+      "This is not a valid triangle!"
+    end
+  end
+end	
